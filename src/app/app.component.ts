f2f9855a885875cf571eefa2d160ea51c80dc892
xoaeb100 <shoaib100aib@gmail.com>
2022-05-20 17:23:27 +0530

    read pdf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import jspdf from 'jspdf';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'read-pdf';
+  data =  [
+    {
+      id: 1,
+      name: 'lorem ipsum',
+      username: 'lorem',
+      email: 'Lorem@april.biz',
+    },
+    {
+      id: 2,
+      name: 'Shoaib',
+      username: 'xoaeb',
+      email: 'shoaib100@gmail.tv',
+    },
+    {
+      id: 3,
+      name: 'Clementine Bauch',
+      username: 'Samantha',
+      email: 'Nathan@yesenia.net',
+    },
+    {
+      id: 4,
+      name: 'Patricia Lebsack',
+      username: 'Karianne',
+      email: 'Julianne.OConner@kory.org',
+    },
+    {
+      id: 5,
+      name: 'Roshan Thakur',
+      username: 'rossshan',
+      email: 'roshanthakur@gmail.com',
+    },
+  ];
+
+  @ViewChild('content') content: any;
+
+  public SavePDF(): void {
+    let content = this.content.nativeElement;
+    let doc = new jspdf();
+
+    let _elementHandlers = {
+      '#editor': function (element: any, renderer: any) {
+        return true;
+      },
+    };
+
+    doc.html(content.innerHTML, {
+      width: 10,
+
+
+      callback: function (doc) {
+        doc.save('test.pdf');
+      },
+    });
+  }
 }
