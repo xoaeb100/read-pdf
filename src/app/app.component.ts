d06b39b986e746c7a4edf1a2eff87a487a004944
xoaeb100 <shoaib100aib@gmail.com>
2022-05-20 18:00:52 +0530

    checkpoint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
 import jspdf from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +9,7 @@ import jspdf from 'jspdf';
 })
 export class AppComponent {
   title = 'read-pdf';
-  data =  [
+  data = [
     {
       id: 1,
       name: 'lorem ipsum',
@@ -41,25 +42,20 @@ export class AppComponent {
     },
   ];
 
-  @ViewChild('content') content: any;
-
-  public SavePDF(): void {
-    let content = this.content.nativeElement;
-    let doc = new jspdf();
-
-    let _elementHandlers = {
-      '#editor': function (element: any, renderer: any) {
-        return true;
-      },
-    };
-
-    doc.html(content.innerHTML, {
-      width: 10,
+  @ViewChild('content') content!: ElementRef;
 
 
-      callback: function (doc) {
-        doc.save('test.pdf');
-      },
+
+  public openPDF(): void {
+    let DATA: any = document.getElementById('content');
+    html2canvas(DATA).then((canvas) => {
+      let fileWidth = 400;
+      let fileHeight = (canvas.height * fileWidth) / canvas.width;
+      const FILEURI = canvas.toDataURL('image/png');
+      let PDF = new jspdf('p', 'mm', 'a4');
+      let position = 0;
+      PDF.addImage(FILEURI, 'PNG', 0, position, fileWidth, fileHeight);
+      PDF.save('angular-demo.pdf');
     });
   }
 }
